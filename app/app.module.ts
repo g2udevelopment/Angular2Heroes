1304acb8dacdf2e58500934da888fa4d23d5d33d
Gerben van de Wiel <cgw@bakkerbarendrecht.nl>
2016-09-12 22:10:59 +0200

    app component toegevoegd.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,9 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-@NgModule({
-  imports:      [ BrowserModule ]
-})
-export class AppModule { }
+  import { NgModule }      from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { AppComponent }   from './app.component';
+  @NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+  })
+  export class AppModule { }
