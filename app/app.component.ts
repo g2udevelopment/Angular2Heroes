ca4047813b199804c2d6e4342e6685082e183a7f
Gerben van de Wiel <cgw@bakkerbarendrecht.nl>
2016-09-14 08:50:00 +0200

    component refactor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-app',
@@ -10,14 +10,7 @@ import { Component } from '@angular/core';
           <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
-  <div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name">
-  </div>
-  </div>`,
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>`,
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -76,11 +69,6 @@ export class AppComponent {
   onSelect(hero: Hero): void {
       this.selectedHero = hero;
   }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
